a24fdd381f9945ae345ff5d3578bf0d8d3603126
samueltoh <samueltoh93@gmail.com>
2022-01-03 20:49:43 +0800

    [DETAILS] Adding in navigation to details page

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -1,4 +1,4 @@
-import { Card, CardActions, CardContent} from "@mui/material";
+import { Button, Card, CardActions, CardContent } from "@mui/material";
 import { styled } from "@mui/system";
 import { Img } from "react-progressive-loader";
 import Text, { TypographyVariant } from "../text";
@@ -6,8 +6,9 @@ import PostReview from "./post_review";
 import PostFoodList from "./post_food_list";
 import PostHashTags from "./post_hash_tags";
 import PostTitle from "./post_title";
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 import { PostDetailContext } from "../../contexts/post_details_context";
+import { Link } from "react-router-dom";
 
 const StyledCard = styled(Card)({
     top: -30,
@@ -26,9 +27,54 @@ const StyledCardActions = styled(CardActions)({
     paddingBottom: '5px'
 })
 
+const StyledButton = styled(Button)({
+    float: 'right',
+    marginBottom: '13px'
+})
+
 const Post = () => {
 
     const postContext = useContext(PostDetailContext)
+
+    const renderDetails = () => {
+        return (<Fragment>
+            <PostReview
+                title={"Positives"}
+                reviews={postContext.post.positives}
+            />
+            <PostReview
+                title={"Neutrals"}
+                reviews={postContext.post.neutrals}
+            />
+            <PostReview
+                title={"Negatives"}
+                reviews={postContext.post.negatives}
+            />
+            {
+                postContext.post.positives === null &&
+                postContext.post.neutrals === null &&
+                postContext.post.negatives === null &&
+                <Text
+                    variant={TypographyVariant.body2}
+                    text={postContext.post.description}
+                />
+            }
+            <PostFoodList />
+        </Fragment>)
+    }
+
+    const generateDetailsButtonLink = () => {
+        return (<Fragment>
+            <Link
+                style={{ display: "block", margin: "1rem 0" }}
+                to={`/food/${postContext.post.id}`}
+                key={postContext.post.id}
+            >
+                <StyledButton size="small">See More</StyledButton>
+            </Link>
+        </Fragment>
+        )
+    }
 
     return (
         <StyledCard key={postContext.post.id} sx={{ maxWidth: 500 }}>
@@ -39,33 +85,18 @@ const Post = () => {
                 />
             }
             <CardContent>
-                <PostTitle/>
-                <PostReview
-                    title={"Positives"}
-                    reviews={postContext.post.positives}
-                />
-                <PostReview
-                    title={"Neutrals"}
-                    reviews={postContext.post.neutrals}
-                />
-                <PostReview
-                    title={"Negatives"}
-                    reviews={postContext.post.negatives}
-                />
+                <PostTitle />
                 {
-                    postContext.post.positives === null &&
-                    postContext.post.neutrals === null &&
-                    postContext.post.negatives === null &&
-                    <Text
-                        variant={TypographyVariant.body2}
-                        text={postContext.post.description}
-                    />
+                    postContext.isPostDetails ?
+                        renderDetails() :
+                        generateDetailsButtonLink()
                 }
-                <PostFoodList/>
             </CardContent>
-            <StyledCardActions>
-                <PostHashTags/>
-            </StyledCardActions>
+            {
+                postContext.isPostDetails && <StyledCardActions>
+                    <PostHashTags />
+                </StyledCardActions>
+            }
         </StyledCard>
     )
 }
