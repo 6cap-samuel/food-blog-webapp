5bc4907a4fcd835deaed3227c4803a2bce5affb2
samueltoh <samueltoh93@gmail.com>
2022-01-23 10:21:03 +0800

    [API_CHANGE] Change API to point to k8s API

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,7 +1,7 @@
-export const BASE_URL = `https://samuel-food-blog-post-service.herokuapp.com`
-// export const BASE_URL = `http://localhost:8081`
-export const POST_URL = `${BASE_URL}/post`
-export const HASHTAG_URL = `${BASE_URL}/hashtag`
+export const BASE_URL = `http://api.henhaochi.io`
+
+export const POST_URL = `${BASE_URL}/posts`
+export const HASHTAG_URL = `${BASE_URL}/hashtags`
 
 export const RETRIEVE_ALL_POST_KEY = "usePostListing"
 export const RETRIEVE_ALL_HASH_TAG_KEY = "useHashtagListing"
