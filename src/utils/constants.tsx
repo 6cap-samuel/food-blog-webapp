dda4152317d7f47151acfbc1c7dd32afd5c09a4d
samueltoh <samueltoh93@gmail.com>
2022-01-26 19:56:55 +0800

    [BUG] Renaming authentication url

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -2,7 +2,7 @@ export const BASE_URL = `https://api.henhaochi.io`
 
 export const POST_URL = `${BASE_URL}/posts`
 export const HASHTAG_URL = `${BASE_URL}/hashtags`
-export const AUTH_URL = `${BASE_URL}/auth`
+export const AUTH_URL = `${BASE_URL}/auth/login`
 
 export const RETRIEVE_ALL_POST_KEY = "usePostListing"
 export const RETRIEVE_ALL_HASH_TAG_KEY = "useHashtagListing"
