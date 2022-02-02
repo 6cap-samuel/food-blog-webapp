ce4b99135590cc30c653ea85335b168830b2c241
samueltoh <samueltoh93@gmail.com>
2022-02-02 13:24:50 +0800

    [ENHANCEMENT] Adding in option to add a new post

--- src/screens/admin/posts/index.tsx
+++ src/screens/admin/posts/index.tsx
@@ -1,12 +1,23 @@
-import { Container } from "@mui/material";
+import { Chip, Container, Fab, Paper } from "@mui/material";
 import { styled } from "@mui/system";
 import { Fragment, useContext } from "react"
 import { AdminPost } from "../../../components/post";
 import { PostContext } from "../../../contexts/post_context";
 import { PostDetailProvider } from "../../../contexts/post_details_context";
+import AddIcon from '@mui/icons-material/Add';
 
 const StyledContainer = styled(Container)({
     marginBottom: '70px'
+})
+
+const StyledPaper = styled(Paper)({
+    textAlign: 'center',
+    paddingTop: 20,
+    paddingBottom: 20
+})
+
+const StyledChip = styled(Chip)({
+    backgroundColor: 'white'
 })
 
 const AdminPostScreen = () => {
@@ -15,6 +26,12 @@ const AdminPostScreen = () => {
 
     return <Fragment>
         <StyledContainer>
+            <StyledPaper>
+                <StyledChip
+                    icon={<AddIcon />} 
+                    label="Add New Post"
+                />
+            </StyledPaper>
             {
                 postContext.postApiListing.data.data.data.map((post) => {
                     return <Fragment key={post.id}>
